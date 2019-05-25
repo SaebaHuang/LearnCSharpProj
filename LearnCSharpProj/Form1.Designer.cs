0d2e2d73df61f44be067ccbc3101741586ea1983
Saeba <1029089917@qq.com>
2019-05-25 21:23:30 +0800

    :tada:  Hello World

--- LearnCSharpProj/Form1.Designer.cs
+++ LearnCSharpProj/Form1.Designer.cs
@@ -28,12 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.WelcomeLabel = new System.Windows.Forms.Label();
+            this.Display = new System.Windows.Forms.Button();
+            this.Quit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // WelcomeLabel
+            // 
+            this.WelcomeLabel.AutoSize = true;
+            this.WelcomeLabel.Font = new System.Drawing.Font("宋体", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.WelcomeLabel.Location = new System.Drawing.Point(21, 81);
+            this.WelcomeLabel.Name = "WelcomeLabel";
+            this.WelcomeLabel.Size = new System.Drawing.Size(0, 9);
+            this.WelcomeLabel.TabIndex = 0;
+            // 
+            // Display
+            // 
+            this.Display.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Display.Location = new System.Drawing.Point(23, 150);
+            this.Display.Name = "Display";
+            this.Display.Size = new System.Drawing.Size(97, 38);
+            this.Display.TabIndex = 1;
+            this.Display.Text = "Display";
+            this.Display.UseVisualStyleBackColor = true;
+            this.Display.Click += new System.EventHandler(this.Display_Click);
+            // 
+            // Quit
+            // 
+            this.Quit.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Quit.Location = new System.Drawing.Point(145, 150);
+            this.Quit.Name = "Quit";
+            this.Quit.Size = new System.Drawing.Size(97, 38);
+            this.Quit.TabIndex = 2;
+            this.Quit.Text = "Quit";
+            this.Quit.UseVisualStyleBackColor = true;
+            this.Quit.Click += new System.EventHandler(this.Quit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.Quit);
+            this.Controls.Add(this.Display);
+            this.Controls.Add(this.WelcomeLabel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label WelcomeLabel;
+        private System.Windows.Forms.Button Display;
+        private System.Windows.Forms.Button Quit;
     }
 }
 
--- LearnCSharpProj/Form1.cs
+++ LearnCSharpProj/Form1.cs
@@ -16,5 +16,20 @@ namespace LearnCSharpProj
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Display_Click(object sender, EventArgs e)
+        {
+            WelcomeLabel.Text = "Welcome to my first .Net framwork App!";
+        }
+
+        private void Quit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
